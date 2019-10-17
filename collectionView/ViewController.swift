260243e1956b2d246bf06cdd6defa126dd45e1e8
Alexandr Honcharenko <alex.devios93@gmail.com>
2019-10-17 17:51:28 +0300

    Initial Commit

---
+++ collectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionView
+//
+//  Created by Alexandr Honcharenko on 17.10.2019.
+//  Copyright © 2019 Alexandr Honcharenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
